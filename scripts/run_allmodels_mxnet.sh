bc70b901a0b6dfc9137a5ddde1b56de32f36588d
Abdul Dakkak <dakkak@illinois.edu>
2019-06-13 19:57:28 +0000

    work

--- scripts/run_allmodels_mxnet.sh
+++ scripts/run_allmodels_mxnet.sh
@@ -4,4 +4,9 @@ export MXNET_CUDA_ALLOW_TENSOR_CORE=0
 export TF_DISABLE_CUDNN_TENSOR_OP_MATH=0
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 
-python main.py --debug --backend=mxnet
+
+for i in {0..29}
+do
+   echo "infer using model $i"
+   python main.py --debug --backend=mxnet --model_idx=$i >> output
+done
