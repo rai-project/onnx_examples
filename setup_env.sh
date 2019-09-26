e07e969f19afcaf89d3d099cf685b561cb31c743
Abdul Dakkak <dakkak@illinois.edu>
2019-09-26 20:27:19 +0000

    update results and readme

---
+++ setup_env.sh
@@ -0,0 +1,11 @@
+export MXNET_CUDA_ALLOW_TENSOR_CORE=0
+export TF_DISABLE_CUDNN_TENSOR_OP_MATH=0
+
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
+export TF_CUDNN_USE_AUTOTUNE=0
+
+export MXNET_EXEC_BULK_EXEC_INFERENCE=0
+export MXNET_EXEC_BULK_EXEC_TRAIN=0
+
+# clear LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:/home/ubuntu/.gvm/pkgsets/go1.12/global/overlay/lib
