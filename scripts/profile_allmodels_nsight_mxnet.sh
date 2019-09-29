fcf5db4408075208410ac957190e2d27a7b45e8e
Abdul Dakkak <dakkak@illinois.edu>
2019-09-29 12:41:46 +0000

    update

--- scripts/profile_allmodels_nsight_mxnet.sh
+++ scripts/profile_allmodels_nsight_mxnet.sh
@@ -2,6 +2,8 @@
 
 trap "exit" INT
 
+sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 export MXNET_CUDA_ALLOW_TENSOR_CORE=0
@@ -29,6 +31,8 @@ HOST_NAME=$(hostname)
 GPU_NAME=$(nvidia-smi --query-gpu="name" --format=csv | sed -n 2p | tr -s ' ' | tr ' ' '_')
 RESULTS_DIR=profile_results/mxnet/nsight/${GPU_NAME}
 
+NSYS=/opt/nvidia/nsight-systems/2019.5.1/bin/nsys
+
 mkdir -p ${RESULTS_DIR}
 nvidia-smi -x -q -a >${RESULTS_DIR}/nvidia_smi.xml
 
@@ -47,22 +51,22 @@ for BATCH_SIZE in "${batch_sizes[@]}"; do
         # run mxnet models instead of onnx models for batch size > 1 for some models
         if [[ "$BATCH_SIZE" -ne 1 ]]; then
             if [[ "$i" -eq 1 ]]; then # alexnet
-                nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python mxnet/gluon_forward.py ${BATCH_SIZE_OPT} --model_name=alexnet --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
+                ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python mxnet/gluon_forward.py ${BATCH_SIZE_OPT} --model_name=alexnet --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
                 continue
             elif [[ "$i" -eq 2 ]]; then # bvlc_caffenet
-                nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=bvlc_caffenet --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
+                ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=bvlc_caffenet --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
                 continue
             elif [[ "$i" -eq 3 ]]; then # bvlc_googlenet
-                nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=bvlc_googlenet --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
+                ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=bvlc_googlenet --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
                 continue
             elif [[ "$i" -eq 4 ]]; then # bvlc_rcnn_ilsvrc13
-                nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=bvlc_rcnn_ilsvrc13 --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
+                ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=bvlc_rcnn_ilsvrc13 --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
                 continue
             elif [[ "$i" -eq 8 ]]; then # inception_v1
-                nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=inception_v1 --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
+                ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=inception_v1 --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
                 continue
             elif [[ "$i" -eq 29 ]]; then # zfnet512
-                nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=zfnet512 --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
+                ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python mxnet/local_forward.py ${BATCH_SIZE_OPT} --model_name=zfnet512 --model_idx=$i --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS
                 continue
             elif [[ ("$i" -eq 7) || ("$i" -eq 9) ]]; then
                 continue
@@ -71,15 +75,15 @@ for BATCH_SIZE in "${batch_sizes[@]}"; do
 
         # run onnx models
         if [[ "$i" -eq 0 ]]; then # arcface
-            nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=112
+            ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=112
         elif [[ "$i" -eq 7 ]]; then # emotion_ferplus
-            nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=64 --input_channels=1
+            ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=64 --input_channels=1
         elif [[ "$i" -eq 10 ]]; then # mnist
-            nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=28 --input_channels=1
+            ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=28 --input_channels=1
         elif [[ "$i" -eq 24 ]]; then # tiny_yolo
-            nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=416
+            ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i --input_dim=416
         else
-            nsys profile --trace=cuda,cudnn,cublas --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i
+            ${NSYS} profile --trace=cuda,cudnn,cublas,nvtx --output=${NSIGHT_PATH} --export=sqlite python main.py ${BATCH_SIZE_OPT} --backend=mxnet --short_output --num_warmup=$NUM_WARMUP --num_iterations=$NUM_ITERATIONS --model_idx=$i
         fi
     done
 done
