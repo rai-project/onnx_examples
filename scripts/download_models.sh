98798f6d4513fd65ef9160d57d19495708d77462
Abdul Dakkak <dakkak@illinois.edu>
2019-09-30 19:50:41 +0000

    update run script

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+pyenv virtualenv miniconda3-4.3.30 dlperf
+pyenv activate dlperf
+
+pip install onnx onnxmltools
+pip install future click numba
+pip install onnxruntime-gpu
+pip install mxnet-cu101mkl
+pip install gluoncv
+
 mc cp -r s3/store.carml.org/dlperf/mxnet_models ./mxnet
 
-mc cp -r s3/store.carml.org/dlperf/onnx_models/* ~/data/carml/dlperf/
+mc cp -r s3/store.carml.org/dlperf/onnx_models/ ~/data/carml/dlperf/
