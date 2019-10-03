8e263b3b2d27b5217cec60004cbc651009db8e9b
Abdul Dakkak <dakkak@illinois.edu>
2019-10-03 01:32:18 +0000

    work

--- scripts/combo.sh
+++ scripts/combo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./scripts/profile_allmodels_nsight_caffe2.sh
+./scripts/profile_allmodels_cudnn_caffe2.sh
 
-./scripts/profile_allmodels_nsight_onnxruntime.sh
+./scripts/profile_allmodels_cublas_caffe2.sh
